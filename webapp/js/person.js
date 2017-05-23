5078b9f5a91d3a12ebf2503139ef47c60eb81933
MMS-RW <693822317@qq.com>
2017-05-23 16:22:49 +0800

    测试分支提交

---
+++ webapp/js/person.js
@@ -0,0 +1,7 @@
+require(['config'],function(){
+    require(['jquery'],function($){
+        $('#Htop').load('../html/personal-header.html');
+        $('#Hfoot').load('../html/personal-footer.html');
+
+});
+});
