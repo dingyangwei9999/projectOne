32521e40aa49dcb467292eb48562b8569b90fbec
MMS-RW <693822317@qq.com>
2017-05-24 12:11:38 +0800

    第三天早上写的东西

--- webapp/js/person.js
+++ webapp/js/person.js
@@ -5,7 +5,10 @@ require(['config'],function(){
         	return $('.h_2').html($Htitle);
         });
         $('.Hfoot').load('../html/personal-footer.html');
-        
+        //点击三个导航栏的时候变色
+        $('.tab-ul').on('click','>li',function(){
+        	$(this).addClass('dibu-color').siblings().removeClass('dibu-color');
+        })
 
 });
 });
