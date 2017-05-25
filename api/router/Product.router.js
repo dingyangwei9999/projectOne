7e1d08f1ba449a627989010960d260f258d70ce0
wei <921869261@qq.com>
2017-05-25 09:26:35 +0800

    5.25

--- api/router/Product.router.js
+++ api/router/Product.router.js
@@ -1,13 +1,17 @@
 
-
-exports.handle = function(app){
-	app.get('/getproduct',function(req,res){
-		res.send('yes');
-	})
-	app.get('/getproductbyid',function(req,res){
-		res.send('yes');
-	})
-	app.get('/getproductbyname',function(req,res){
-		res.send('yes');
-	})
-}
+// exports.handle = function(app){
+// 	app.get('/getproduct',function(req,res){
+// 		res.send('yes');
+// 	})
+// 	app.get('/getproductbyid',function(req,res){
+// 		res.send('yes');
+// 	})
+// 	app.get('/getproductbyname',function(req,res){
+// 		res.send('yes');
+// 	})
+// }
+app.get('/getproduct',function(req,res){
+		//console.log(req.body)
+		dbIndex.getIndexData('index', req.body, res);
+		//res.send('yes');
+	});
